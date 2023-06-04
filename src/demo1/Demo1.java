7fc4d624c9c1579edb1d8bb5c0613240e12c45fa
rushikeshmhase7 <132046401+rushikeshmhase7@users.noreply.github.com>
2023-06-04 11:37:30 +0530

    Update Demo1.java

--- src/demo1/Demo1.java
+++ src/demo1/Demo1.java
@@ -3,6 +3,7 @@ package demo1;
 public class Demo1 {
 public static void main(String[] args) {
 	System.out.println("Welcome to wish page");
+	System.out.println("++++++++++++++++++++");
 	AnniversaryWishes a=new AnniversaryWishes();
 	a.wish();
 	BirthdayWishes b=new BirthdayWishes();
